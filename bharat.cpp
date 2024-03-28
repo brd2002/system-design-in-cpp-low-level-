278ff6ddc4f48cdc89d1babef6cd348ab544aa22
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-28 14:00:25 +0530

    1st lecture is completed

--- bharat.cpp
+++ bharat.cpp
@@ -4,6 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    long long int a = 10 ;
     cout << "this is system design folder...." << endl;
     vector<int>s ;
     s.push_back(10) ;
