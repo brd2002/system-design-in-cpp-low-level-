bad85c49ed2393ca28befd562228768946e7ac09
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-28 13:03:52 +0530

    this is my first commit

---
+++ bharat.cpp
@@ -0,0 +1,12 @@
+//
+// Created by bhara on 27-03-2024.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "this is system design folder...." << endl;
+    vector<int>s ;
+    s.push_back(10) ;
+    s.pop_back() ;
+
+}
